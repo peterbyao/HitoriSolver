32699fdeabbd1f6e58d461f461bf3c1a5083b3b8
peterbyao <42585479+peterbyao@users.noreply.github.com>
2023-12-19 14:27:05 -0500

    Add files via upload

---
+++ submission/CDCL.hs
@@ -0,0 +1,171 @@
+{-
+
+ Names: Peter Yao and Ava Hajratwala
+ Uni: pby2101 and ash2261
+
+ ------------------------------
+
+ COMS 4995 001 Parallel Functional Programming
+
+ Final Project: Hitori Solver
+
+ CDCL implementation is based on https://github.com/tamionv/HaskSat/blob/master/README.md
+
+ Only changes made here are to reduce warnings.
+-}
+
+module CDCL (solveCDCL
+                , Var
+                , Lit
+                , Clause
+                , Formula ) where
+
+import Control.Monad
+import Control.Applicative
+import Control.Conditional (ifM)
+import Control.Monad.State
+import Data.List
+import Data.Ord
+import Data.Maybe
+import qualified Data.IntMap as M
+import Data.Bifunctor (second)
+
+type Var = Int
+
+type Lit = Int
+
+type Clause = [Lit]
+
+type Formula = [Clause]
+
+data Maplet = Maplet { value :: !Bool
+                     , reason :: !(Maybe Clause)
+                     }
+
+type Assignment = M.IntMap Maplet
+
+type AlgState = (Formula, Assignment)
+
+newtype AlgMonad x = AlgMonad { runMonad :: forall r. AlgState -> (AlgState -> x -> r) -> (Formula -> r) -> r }
+
+instance Monad AlgMonad where
+    xm >>= f = AlgMonad $ \s@(_, _) k kf -> runMonad xm s (\(!s') (!x) -> runMonad (f x) s' k kf) kf
+
+instance MonadPlus AlgMonad where
+    mzero = AlgMonad $ \(!f, _) _ kf -> kf f
+
+    xm `mplus` ym = AlgMonad $ \s@(_, a) k kf ->
+        runMonad xm s k (\(!f') -> runMonad ym (f', a) k kf)
+
+instance MonadState AlgState AlgMonad where
+    get = AlgMonad $ \(!s) k _ -> k s s
+    put s = AlgMonad $ \_ k _ -> k s ()
+
+instance Functor AlgMonad where
+    fmap :: (a -> b) -> AlgMonad a -> AlgMonad b
+    fmap = liftM
+
+instance Applicative AlgMonad where
+    (<*>) :: AlgMonad (a -> b) -> AlgMonad a -> AlgMonad b
+    (<*>) = ap
+    pure x = AlgMonad $ \(!s) k _ -> k s x
+
+instance Alternative AlgMonad where
+    empty = mzero
+    (<|>) = mplus
+
+choices :: Assignment -> [Lit]
+choices !a = map interpret $ M.assocs a where
+    interpret (v, mp) | value mp = v
+                      | otherwise = -v
+
+varOfLit :: Lit -> Var
+varOfLit !l = abs l
+
+polarityOfLit :: Lit -> Bool
+polarityOfLit !l = l > 0
+
+addMaplet :: Lit -> Maybe Clause -> Assignment -> Assignment
+addMaplet !l !r !a = M.insert v m a where
+    v = varOfLit l
+    m = Maplet { value = polarityOfLit l
+               , reason = r
+               }
+
+isMentioned :: Lit -> Assignment -> Bool
+isMentioned !l !a = isJust $ M.lookup (varOfLit l) a
+
+lookupLit :: Lit -> Assignment -> Maybe Bool
+lookupLit !l !a = (==polarityOfLit l) . value <$> M.lookup (varOfLit l) a
+
+clauseAfterAssignment :: Assignment -> Clause -> Maybe Clause
+clauseAfterAssignment !a !c
+    | isSat = Nothing
+    | otherwise = Just simplified where
+        isSat = or $ mapMaybe (`lookupLit` a) c
+        simplified = filter (not . (`isMentioned` a)) c
+
+formulaAfterAssignment :: Formula -> Assignment -> Formula
+formulaAfterAssignment !f !a = mapMaybe (clauseAfterAssignment a) f
+
+unitPropagation :: AlgState -> AlgState
+unitPropagation (!f, !a) = (f, a') where
+    lits = nubBy (\(x, _) (y, _) -> varOfLit x == varOfLit y)
+         $ map filterX
+         $ filter ((==1) . length . fst)
+         $ mapMaybe (\c -> do x <- clauseAfterAssignment a c; return (x, c)) f
+    filterX ([x], y) = (x, Just $ filter (/=x) y)
+    filterX _ = error "Unit Propagation Error"
+    a' = foldr (uncurry addMaplet) a lits
+
+addClause :: Clause -> AlgState -> AlgState
+addClause !c (!f, !a) = (c:f, a)
+
+chooseLit :: AlgState -> Lit
+chooseLit (!f, !a) = head
+                   $ minimumBy (comparing length)
+                   $ formulaAfterAssignment f a
+
+learnedClauses :: AlgState -> [Clause]
+learnedClauses (!f, !a) = map getConflict
+                        $ take 1
+                        $ filter ((==Just []) . clauseAfterAssignment a) f where
+    getConflict [] = []
+    getConflict (c:cs) = case M.lookup (varOfLit c) a >>= reason of
+        Just r -> getConflict $ r ++ cs
+        Nothing -> c : getConflict cs
+
+satisfied :: AlgState -> Bool
+satisfied (!f, !a) = null (formulaAfterAssignment f a)
+
+unsatisfied :: AlgState -> Bool
+unsatisfied (!f, !a) = elem [] $ formulaAfterAssignment f a
+
+hasUnit :: AlgState -> Bool
+hasUnit (!f, !a) = any ((==1) . length) $ formulaAfterAssignment f a
+
+algorithmAction :: AlgMonad [Lit]
+algorithmAction = go where
+    go = get >>= mainCond
+
+    mainCond s
+        | satisfied s = gets (choices . snd)
+        | unsatisfied s = learnAndFail
+        | hasUnit s = unitProp
+        | otherwise = tryLiteral
+
+    unitProp = do modify' unitPropagation ; go
+
+    learnAndFail = do
+        cs <- gets learnedClauses
+        forM_ cs $ \c -> unless (null c) $ modify $ addClause c
+        mzero
+
+    tryLiteral = gets chooseLit >>= exhaust
+
+    choose lit = do modify' $ Data.Bifunctor.second (addMaplet lit Nothing) ; go
+
+    exhaust lit = choose lit `mplus` ifM (gets $ not . unsatisfied) (choose (-lit)) mzero
+
+solveCDCL :: Formula -> Maybe [Lit]
+solveCDCL f = runMonad algorithmAction (f, M.empty) (\_ x -> Just x) (const Nothing)
---
+++ submission/CubeAndConquer.hs
@@ -0,0 +1,195 @@
+{-
+ Names: Peter Yao and Ava Hajratwala
+ Uni: pby2101 and ash2261
+
+ ------------------------------
+
+ COMS 4995 001 Parallel Functional Programming
+
+ Final Project: Hitori Solver
+
+ DPLL Implementation is based on https://gist.github.com/gatlin/1755736
+ CDCL Implementation is based on https://github.com/tamionv/HaskSat/blob/master/README.md
+ Lookahead logic inspired by Marijn Heule (References listed in Final Report)
+
+ We modify the DPLL implementation to include lookahead logic. Finally, we add import CDCL and
+ use it to solve a cube of reduced sub-problems.
+-}
+
+module CubeAndConquer (solveCube, getInitCube) where
+import Data.Maybe
+import Data.Set (toList, fromList, Set, (\\))
+import Control.Parallel.Strategies
+import Control.Monad (msum)
+import CDCL (solveCDCL)
+
+type Literal = Int
+type Clause  = [Literal]
+type Formula = [Clause]
+type Record  = [Literal]
+
+
+-- The state of a solver at any given time is a subset of the original formula and a record of assignments
+data SolverState = SolverState { formula :: !Formula
+                               , record  :: !Record
+                               } deriving (Show)
+type Cube = [SolverState]
+
+-- DList datatype is introduced to flatten the implication graph
+newtype DList a = DList ([a] -> [a])
+
+instance Semigroup (DList a) where
+    DList xs <> DList ys = DList (xs . ys)
+
+instance Monoid (DList a) where
+    mempty = DList id
+
+singleton :: a -> DList a
+singleton a = DList (a:)
+
+dListToList :: DList a -> [a]
+dListToList (DList f) = f []
+
+--Get cube using a lookahead depth
+getCube :: SolverState -> Int-> DList SolverState
+getCube (SolverState f r) i = case chooseLookaheadLit f of
+                                        Nothing -> mempty
+                                        Just l -> if i == 0 then 
+                                                        case (pos, neg) of
+                                                            (Nothing, Nothing) -> mempty
+                                                            (Nothing, Just n)  -> singleton n
+                                                            (Just p, Nothing)  -> singleton p
+                                                            (Just p, Just n)   -> singleton p <> singleton n
+                                                    else
+
+                                                        case (pos, neg) of
+                                                            (Nothing, Nothing) -> mempty
+                                                            (Nothing, Just n)  -> getCube n (i-1)
+                                                            (Just p, Nothing)  -> getCube p (i-1)
+                                                            (Just p, Just n)   -> getCube p (i-1) <> getCube n (i-1)
+
+                                                    where 
+                                                        pos = identifyConflict $ (SolverState (simplify f l) (l:r))
+                                                        neg = identifyConflict $ (SolverState (simplify f (-l)) ((-l):r))
+
+
+-- Take all the cubes and try to solve them with CDCL
+conquerCubes :: Cube -> Maybe [Literal]
+conquerCubes cube = case cube of
+                        [] -> Nothing
+                        cs -> msum $ runEval $ parMap' solveCDCL cubeFormula
+                                where cubeFormula = map ((filter (not . null)) . solverToFormula) cs
+
+
+
+{-
+identifyConflict checks if simplified formula contains any conflicts. The original function was
+unitpropagate, which simplified the formula but did not check for conflicts.
+-}
+
+-- Function takes a solverstate and returns Nothing if a conflict is detected.
+identifyConflict :: SolverState -> Maybe SolverState
+identifyConflict (SolverState f r) = if conflict f then Just (SolverState f r)
+                                        else case getUnit f of
+                                            Nothing -> Nothing
+                                            Just u  -> identifyConflict $ SolverState (simplify f u) (u:r)
+
+
+-- Given a list, check to see whether or not unit clauses contain conflicts.
+conflict :: Formula -> Bool
+conflict f = case [x | [x] <- f'] of
+                    []    -> True
+                    units -> ((length $ fromList (map abs units)) < (length $ units))
+                where f' = (toList . fromList) f
+            
+
+
+-- Pop to get the top of the records
+pop :: Record -> Maybe (Int, Record)
+pop [] = Nothing
+pop (x:xs) = Just (x, xs)
+
+
+--SolverState to formula
+solverToFormula :: SolverState -> Formula
+solverToFormula (SolverState f r) = case pop r of
+                                        Nothing -> f
+                                        Just (u, r') -> solverToFormula (SolverState ([u]:f) r')
+
+
+chooseLookaheadLit :: Formula -> Maybe Literal
+chooseLookaheadLit !f = case reductionPar f of
+                            [] -> Nothing
+                            xs -> Just (snd $ maximum xs)
+
+--Gets a list of variables remaining in the formula
+getVariables :: Formula -> Set Int
+getVariables f = fromList (Prelude.map abs (concat f))
+
+{- 
+Sequential measure of reduction 
+
+--Measures the number of reduced clauses. We want this to be as large as possible.
+measureReduction :: Formula -> [(Int, Int)]
+measureReduction f = zip [length (Data.Set.fromList(simplify f l) \\ s) + 
+                                length (Data.Set.fromList(simplify f (-l)) \\ s) 
+                                | l <- vars] vars
+                                        where 
+                                            vars = toList $ getVariables f
+                                            s = Data.Set.fromList f
+-}  
+
+
+parMap' :: (a -> b) -> [a] -> Eval [b]
+parMap' _ [] = return []
+parMap' f (a:as) = do
+                    b <- rpar (f a)
+                    bs <- parMap' f as
+                    return (b:bs)
+
+
+
+--Parallel measure of reduction
+reductionPar :: Formula -> [(Int, Int)]
+reductionPar f = zip (runEval $ parMap' (function f) vars) vars
+                --zip (force $ parMap rpar (function f) vars) vars
+                        where
+                            vars = toList $ getVariables f
+                            s = Data.Set.fromList f
+                            function :: Formula -> Int -> Int
+                            function f' l = (length (Data.Set.fromList(simplify f' l) \\ s) + 
+                                            length (Data.Set.fromList(simplify f' (-l)) \\ s))
+
+
+{-
+reductionSeq :: Formula -> [(Int, Int)]
+reductionSeq f = [(length (Data.Set.fromList(simplify f l) \\ s) + 
+                        length (Data.Set.fromList(simplify f (-l)) \\ s), l) 
+                        | l <- vars]
+                    where 
+                        vars = toList $ getVariables f
+                        s = Data.Set.fromList f
+-}
+
+-- If a unit clause (singleton list) exists in the formula, return the literal inside it, or Nothing.
+getUnit :: Formula -> Maybe Literal
+getUnit !xs = listToMaybe [ x | [x] <- xs ]
+
+-- Simplifying a formula wrt a literal means, for every clause in which `-l` is a member remove `-l`, 
+-- and remove every clause from f which contains `l`.
+simplify :: Formula -> Literal -> Formula
+simplify !f !l = [ simpClause x l | x <- f, not (elem l x) ]
+    where
+        simpClause c l' = Prelude.filter (/= -l') c
+
+--Depth parameter
+depth :: Int
+depth = 0
+
+-- Solve a formula with the Cube and Conquer method
+solveCube :: [[Int]] -> Maybe [Int]
+solveCube f = conquerCubes $ dListToList $ (getCube (SolverState f []) depth)
+
+-- Exported function to display the cube
+getInitCube :: [[Int]] -> Cube
+getInitCube f = dListToList $ (getCube (SolverState f []) depth)
